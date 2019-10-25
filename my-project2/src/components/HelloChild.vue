c1cde9168c621e8a942f16849d4d56a61310bb89
田島 圭 <13041@kumahira.jp>
2019-10-25 20:08:48 +0900

    vue.jsをいろいろいじってみた

---
+++ my-project2/src/components/HelloChild.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+  <p>{{title}}</p>
+  <!-- ボタンが押されるとincrementCounterが実行される -->
+  <button v-on:click="incrementCounter">{{ counter }}</button>
+ </div>
+</template>
+ 
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'はろー！こども！',
+      counter: 0
+    }
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1
+      // 親コンポーネントのincrementが呼び出す（実行）
+      this.$emit('increment')
+    }
+  }
+}
+</script>
--- my-project2/src/components/HelloWorld.vue
+++ my-project2/src/components/HelloWorld.vue
@@ -1,113 +1,28 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+ <div>
+  <p>{{title}}</p>
+  <p>{{ total }}</p>
+  <!-- incrementと言うイベントを定義。子コンポーネントから$emitされたら、incrementTotal実行される -->
+  <hello-child v-on:increment="incrementTotal"/>
+ </div>
 </template>
-
+ 
 <script>
+import HelloChild from './HelloChild'
 export default {
-  name: 'HelloWorld',
-  data () {
+  data: function () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'はろー！わーるど！',
+      total: 0
     }
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total += 1
+    }
+  },
+  components: {
+    HelloChild
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
